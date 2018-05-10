da3ecdc0fd59df41cb98a0a3f311723b9908358c
André Calero Valdez <andrecalerovaldez@gmail.com>
2018-05-11 01:22:15 +0200

    Added RWTH Color palette to github readme

--- tests/testthat/testUtils.R
+++ tests/testthat/testUtils.R
@@ -1,5 +1,7 @@
+# Context is the file that ist tested
 context("utils")
-test_that("test",{
+# name the test
+test_that("binary test",{
   a <- as.factor(c("test", "test2"))
   b <- c(1, 2)
   expect_equal(as.numeric.factor(a), b)
