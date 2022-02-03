66c7e4252ced49013f10ba46d656980b636e297a
André Calero Valdez <andrecalerovaldez@gmail.com>
2022-02-03 10:21:07 +0100

    hotfix for corrplot update

--- R/correlationPlots.R
+++ R/correlationPlots.R
@@ -15,23 +15,53 @@
 cor.matrix.plot <- function(data, conf.level = .95) {
   rwthcolors <- rwth.colorpalette()
   p <- corrplot::cor.mtest(data, conf.level = conf.level)
-  col <- grDevices::colorRampPalette(c(rwthcolors$red, "#FFFFFF", rwthcolors$blue))
-  stats::cor(data, use = "pairwise.complete.obs") %>% corrplot::corrplot( method = "color", col = col(200),
-                                                         type = "upper", order = "hclust", number.cex = .7,
-                                                         addCoef.col = "black", # Add coefficient of correlation
-                                                         tl.col = "black", tl.srt = 90, # Text label color and rotation
-                                                         # Combine with significance
-                                                         p.mat = p$p, sig.level = c(.001, .01, .05), insig = "n",
-                                                         # hide correlation coefficient on the principal diagonal
-                                                         diag = TRUE, tl.pos = "lt")
-  stats::cor(data, use = "pairwise.complete.obs") %>% corrplot::corrplot( method = "color", col = col(200),
-                                                         type = "lower", order = "hclust", number.cex = .7,
-                                                         #addCoef.col = "black", # Add coefficient of correlation
-                                                         #tl.col = "black", tl.srt = 90, # Text label color and rotation
-                                                         # Combine with significance
-                                                         p.mat = p$p, sig.level = c(.001, .01, .05), insig = "label_sig",
-                                                         pch.cex = 0.8,
-                                                         # hide correlation coefficient on the principal diagonal
-                                                         diag = TRUE, add = TRUE, tl.pos = "n")
+  col <-
+    grDevices::colorRampPalette(c(rwthcolors$red, "#FFFFFF", rwthcolors$blue))
+  tryCatch({
+    stats::cor(data, use = "pairwise.complete.obs") %>% corrplot::corrplot(
+      method = "color",
+      col = col(200),
+      type = "upper",
+      order = "hclust",
+      number.cex = .7,
+      addCoef.col = "black",
+      # Add coefficient of correlation
+      tl.col = "black",
+      tl.srt = 90,
+      # Text label color and rotation
+      # Combine with significance
+      p.mat = p$p,
+      sig.level = c(.001, .01, .05),
+      insig = "n",
+      # hide correlation coefficient on the principal diagonal
+      diag = TRUE,
+      tl.pos = "lt"
+    )
+  },
+  error = function(x) {
+  })
+
+  tryCatch({
+    stats::cor(data, use = "pairwise.complete.obs") %>% corrplot::corrplot(
+      method = "color",
+      col = col(200),
+      type = "lower",
+      order = "hclust",
+      number.cex = .7,
+      #addCoef.col = "black", # Add coefficient of correlation
+      #tl.col = "black", tl.srt = 90, # Text label color and rotation
+      # Combine with significance
+      p.mat = p$p,
+      sig.level = c(.001, .01, .05),
+      insig = "label_sig",
+      pch.cex = 0.8,
+      # hide correlation coefficient on the principal diagonal
+      diag = TRUE,
+      add = TRUE,
+      tl.pos = "n"
+    )
+  },
+  error = function(x) {
+  })
 
 }
