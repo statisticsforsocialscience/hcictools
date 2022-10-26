f2b29a5a846930b251d668dc749b5a93d7bbf205
rocknromeo <super-dude@gmx.de>
2022-10-27 00:18:10 +0200

    Multiple awesome bug fixes. The method should work like a charm now :-)

--- R/careless_index.R
+++ R/careless_index.R
@@ -17,19 +17,25 @@
 #' @param id_column A string that contains the name of a id column. As a default
 #' we use "ResponseId", which is used by Qualtrix.
 #' @param antonyms Whether or not to test for psychometric antonyms (default: T)
+#' @param critval_ant Threshold for antonym analysis (default: -0.6)
 #' @param synonyms Whether or not to test for psychometric synonyms (default: T)
+#' @param critval_syn Threshold for synonym analysis (default: -0.6)
+#' @param mahaD Whether or not to test for mahalanobis distance with confidence of 0.99. (default: T)
 #'
 #' @return Returns an updated dataframe containing indices
 #' @export
 #'
 # @examples
 careless_indices <- function(dat,
-                             speeder_analysis = "median/2",
+                             speeder_analysis = F,
                              likert_vector = NULL,
                              duration_column = "Duration (in seconds)",
                              id_column = "ResponseId",
                              antonyms = TRUE,
-                             synonyms = TRUE)  {
+                             critval_ant = -0.6,
+                             synonyms = TRUE,
+                             critval_syn = 0.6,
+                             mahaD = TRUE)  {
 
 
   stopifnot("speeder_analysis must be either \"median\", \"median/2\",\"median/3\", FALSE, or a numeric value." =
@@ -53,15 +59,15 @@ careless_indices <- function(dat,
         speedlimit <-
           stats::median(dat[[duration_column]]) / as.numeric(speedercondition[[1]][2])
       }
-      dat$speeder <-
+      dat$careless_speeder <-
         ifelse(dat[[duration_column]] > speedlimit, FALSE, TRUE)
-      data.quality <- dat[which(dat$speeder == FALSE), ]
+      data.quality <- dat[which(dat$careless_speeder == FALSE), ]
     }
-    # data.quality contains the filtered list
+    # data.quality contains now a filtered list. Further analysis is only performed on non-speeders
 
-    # Ooops will break
     quality.indices <-
-      data.frame(ResponseId = data.quality$ResponseId)
+      data.frame(ResponseId = data.quality[[id]])
+    names(quality.indices)[names(quality.indices) == "ResponseId"] <- id
 
     if (!is.null(likert_vector)) {
       # This works with speeders ----
@@ -70,38 +76,38 @@ careless_indices <- function(dat,
 
       # add longstrings to data frame
       temp <- careless::longstring(df_likerts_numeric, avg = T)
-      dat$longstr <- temp$longstr
-      dat$avgstr <- temp$avgstr
+      dat$careless_longstr <- temp$longstr
+      dat$careless_avgstr <- temp$avgstr
 
       # add inter response variability
-      dat$irv <- careless::irv(df_likerts_numeric)
+      dat$careless_irv <- careless::irv(df_likerts_numeric)
 
 
       # This (sensibly) works only without speeders ----
-      df_likerts_numeric <-
-        dplyr::mutate_all(data.quality[, likert_vector], as.numeric)
       if (synonyms) {
-        tryCatch(quality.indices$psychsyn <-
-              careless::psychsyn(df_likerts_numeric, critval = 0.6),
+        tryCatch(quality.indices$careless_psychsyn <-
+              careless::psychsyn(df_likerts_numeric, critval = critval_syn),
               error = function(e) {
                 warning(e)
               })
       }
       if (antonyms) {
-        tryCatch(quality.indices$psychant <-
+        tryCatch(quality.indices$careless_psychant <-
               careless::psychsyn(df_likerts_numeric, critval = -0.6, anto = T),
               error = function(e) { warning(e) })
       }
-      temp <-
-        careless::mahad(
-          df_likerts_numeric,
-          flag = TRUE,
-          confidence = 0.99,
-          na.rm = TRUE,
-          plot = FALSE
-        )
-      quality.indices$mahadD <- temp$d_sq
-      quality.indices$mahadflag <- temp$flagged
+      if (mahaD) {
+        temp <-
+          careless::mahad(
+            df_likerts_numeric,
+            flag = TRUE,
+            confidence = 0.99,
+            na.rm = TRUE,
+            plot = FALSE
+          )
+        quality.indices$careless_mahadD <- temp$d_sq
+        quality.indices$careless_mahadflag <- temp$flagged
+      }
     }
 
     # join by merging, TODO needs testing
